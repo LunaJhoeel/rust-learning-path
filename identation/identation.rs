a34ed9a9589825ae9fa33ca48015c47fe9a2c3c6
Jhoeel <lunajhoeel@gmail.com>
2024-02-27 16:40:20 -0500

    feat: Add identation and comments in Rust

---
+++ identation/identation.rs
@@ -0,0 +1,15 @@
+fn main(){ // The function declaration is not indented
+
+    // First step.
+        // Substep A: execute before First step can be completed.
+        // Substep B: execute before First step can be completed.
+
+    // Second step.
+        // Substep C: execute before Second step can be completed.
+        // Substep D: execute before Second step can be completed.
+            // Sub-substep 1: execute before Substep D can be completed.
+
+    // Third step
+
+    // ...
+}
