2ebe70ce0819ffffbc634f9bd946dbc84969656e
Jhoeel <lunajhoeel@gmail.com>
2024-02-27 16:55:02 -0500

    feat: Add arguments in strings

---
+++ arguments/SETUP.md
@@ -0,0 +1,10 @@
+# Commands
+
+Compile program
+```bash
+rustc arguments.rs
+```
+Run program
+```bash
+./arguments
+```
---
+++ arguments/arguments.rs
@@ -0,0 +1,8 @@
+fn main (){
+    // Call println! with three arguments: a string, a value, and another value
+    println!(
+        "The first letter of the alphabet is {} and the last letter is {}.",
+        'A',
+        'Z'
+    );
+}
