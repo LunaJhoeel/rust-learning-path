562b7203e11311f8f18dad8aafced4c2c6d5cf8d
Jhoeel <lunajhoeel@gmail.com>
2024-02-27 21:53:06 -0500

    feat: Add variables declaration and binding

---
+++ variables/variables.rs
@@ -0,0 +1,19 @@
+fn main(){
+    // Declare a var
+    let a_number;
+
+    // Declare a second var and bind a value
+    let a_word = "Ten";
+
+    // Bind a value to first var
+    a_number = 10;
+
+    println!(
+        "The number is {}",
+        a_number
+    );
+    println!(
+        "The word is {}",
+        a_word
+    );
+}
